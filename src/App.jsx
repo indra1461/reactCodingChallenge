4ebdad8e2ed83f02f78bc5bb31c82de64778b5bf
indrajeet yadav <yadav1461@gmail.com>
2026-08-16 01:00:52 +0530

    toggle password done

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Counter from "./challenges/01-counter/Counter";
 import TodoList from "./challenges/02-todo/TodoList";
 import SearchTable from "./challenges/03-searchTable/SearchTable";
+import PasswordToggle from "./challenges/04-passwordToggle/PasswordToggle";
 const App = () => {
   return (
     <>
@@ -13,6 +14,7 @@ const App = () => {
       <hr />
       <SearchTable />
       <hr />
+      <PasswordToggle />
       <hr />
       <hr />
       <hr />
---
+++ src/challenges/04-passwordToggle/PasswordToggle.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+const PasswordToggle = () => {
+  const [password, setPassword] = useState("");
+  const [toggle, setToggle] = useState(false);
+
+  const togglePassword = () => {
+    setToggle((prev) => !prev);
+  };
+  return (
+    <div>
+      <h2>Password Toggle</h2>
+      <input
+        type={toggle === false ? "password" : "text"}
+        placeholder="Enter Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={togglePassword}>
+        {toggle === false ? "👁 Show" : "🙈 Hide"}
+      </button>
+    </div>
+  );
+};
+
+export default PasswordToggle;
