3d77f3c659cea40702f5e028715c7aabc2b6a3e4
indrajeet yadav <yadav1461@gmail.com>
2026-08-15 23:09:58 +0530

    01- counter challenge done

---
+++ src/challenges/01-counter/Counter.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+const Counter = () => {
+  const [counter, setCounter] = useState(0);
+
+  const increment = () => {
+    setCounter((prev) => prev + 1);
+  };
+  const decrement = () => {
+    if (counter === 0) return;
+    setCounter((prev) => prev - 1);
+  };
+  const reset = () => {
+    setCounter(0);
+  };
+
+  return (
+    <div>
+      <h2>Counter App</h2>
+
+      <h3>{counter}</h3>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+      <button onClick={reset}>Reset</button>
+    </div>
+  );
+};
+
+export default Counter;
